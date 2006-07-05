b37fa0a38d5bedd0f63ffd4450e5ca61490621d8
groleo <groleo@01921a93-d2f6-0310-add8-e343ee640d08>
2006-07-05 21:37:34 +0000

    add new cli parameter

--- trunk/bee/lib/scheduler/t_sched.cpp
+++ trunk/bee/lib/scheduler/t_sched.cpp
@@ -17,20 +17,20 @@ void Scheduler::Cron::notify_fun (union sigval sigval)
 
 int main( int argc, char* argv[])
 {
-    if( argc <=2 )
+    if( argc <=3 )
     {
-        printf( "%s clients duration[min]\n", argv[0] ) ;
+        printf( "%s clients refresh_time[sec] test_duration[sec]\n", argv[0] ) ;
         return 1 ;
     }
 
     //! add the clients.
     for( int i=1; i<atoi(argv[1]) ; ++i )
-        cron.addTime( i*60 ) ;  //! addTime parameter is in elapsed.
+        cron.addTime( i*atoi(argv[2]) ) ;  //! addTime parameter is in elapsed.
 
-    cron.refresh( 1*60 ) ;      //! refresh time(1 minute).
+    cron.refresh( 1*atoi(argv[2]) ) ;      //! refresh time(1 minute).
     cron.start() ;              //! start the cron.
 
-    while( elapsed <= atoi( argv[2])  )
+    while( elapsed <= atoi( argv[3])  )
     {
         ;
     }
