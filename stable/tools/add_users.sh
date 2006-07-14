b496124cedd4f68ac2986776e6ea385c1f2c766c
groleo <groleo@01921a93-d2f6-0310-add8-e343ee640d08>
2006-07-14 08:55:55 +0000

    MailStore::init fully functionable

--- stable/tools/add_users.sh
+++ stable/tools/add_users.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+HOST=$1
+if [ "$1" == "" ]; then
+    HOST="localhost"
+fi
+
 for(( i=1; i<=1000; i++ )); do
-    cat add_users.nc | sed -e  "s/userIdx/user$i/g" | nc localhost 7000
+    cat add_users.nc | sed -e  "s/userIdx/user$i/g" | nc $HOST 7000
 done
