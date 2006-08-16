446e4c9b7d9b1c1dc95b34ad0ee53dd6fa9f5a7a
groleo <groleo@01921a93-d2f6-0310-add8-e343ee640d08>
2006-08-16 14:56:38 +0000

    works

--- stable/lib/sysinfo/t_sysinfo.cpp
+++ stable/lib/sysinfo/t_sysinfo.cpp
@@ -7,15 +7,19 @@ int main()
 {
     SysInfo r ;
     float one_m, five_m, ften_m;
-    int read, sect, mergeRead ;
-    cout <<"MemFree:" <<r.memFree() <<endl ;
-    cout <<"MemTotal:" <<r.memTotal() <<endl ;
-    cout <<"SwapFree:" <<r.swapFree() <<endl ;
-    cout <<"SwapTotal:" <<r.swapTotal() <<endl ;
-    cout <<"HDD:ReadInfo:" <<endl;
-    r.readInfo("hda1", &read, &sect , &mergeRead);
-    printf("#Reads:%i Sect:%i mergedRead:%i\n", read, sect, mergeRead);
-    r.loadAvg(&one_m, &five_m, &ften_m) ;
-    printf("loadAvg: %f %f %f\n", one_m, five_m, ften_m);
+    unsigned int read, sect, mergeRead ;
+    for(int i=0; i<10; ++i)
+    {
+        cout <<"MemFree:" <<r.memFree() <<endl ;
+        cout <<"MemTotal:" <<r.memTotal() <<endl ;
+        cout <<"SwapFree:" <<r.swapFree() <<endl ;
+        cout <<"SwapTotal:" <<r.swapTotal() <<endl ;
+        cout <<"HDD:ReadInfo:" <<endl;
+        r.readInfo("hda1", &read, &sect , &mergeRead);
+        printf("#Reads:%lu Sect:%lu mergedRead:%lu\n", read, sect, mergeRead);
+        r.loadAvg(&one_m, &five_m, &ften_m) ;
+        printf("loadAvg: %f %f %f\n", one_m, five_m, ften_m);
+        sleep(1);
+    }
     return 0;
 }
