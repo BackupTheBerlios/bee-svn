ce2fb4cf5b2dfff660c1bec89ee84c64ab7a71b0
groleo <groleo@01921a93-d2f6-0310-add8-e343ee640d08>
2006-10-20 13:40:55 +0000

    no more nc files

--- trunk/bee/tools/expunge.sh
+++ trunk/bee/tools/expunge.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
+EXPUNGE="a login userIdx userIdx\r\na select inbox\r\na store 1:* +flags (\deleted)\r\na expunge\r\na logout\r\n"
 
 for(( i=1; i<=100000; i++ )); do
-    cat expunge.nc | sed -e  "s/userIdx/user$i/g" | nc gigi 143
+    echo ${EXPUNGE} | sed -e  "s/userIdx/user$i/g" | nc gigi 143
 done
