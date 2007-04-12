1a3ceac034e45afb474f736222b1c1aad56d56a6
groleo <groleo@01921a93-d2f6-0310-add8-e343ee640d08>
2007-04-12 21:35:38 +0000

    moved gendebug.cpp in gendebug.c

--- trunk/bee/mtrace/gendebug.c
+++ trunk/bee/mtrace/gendebug.c
@@ -1,9 +1,14 @@
 #include <stdio.h>
-int main() {
-    for(int i=1;i<=0xff; ++i)
+#include <stdlib.h>
+int main( int argc, char* argv[]) {
+    int i=0, top=0xf;
+
+    if(argc == 2 ) top = atoi(argv[1]);
+
+    for(i=1;i<=top; ++i)
         printf("MEMINFO: new() %#.7x 00 unix/thread.cpp(000){00000, -0000000000}\n",
         i);
-    for(int i=1;i<=0xff; ++i)
+    for(i=1;i<=top; ++i)
         printf("MEMINFO: del() %#.7x ../include/common/halloc.h(000){00000, -0000000000}\n",
         i);
         return 0;
