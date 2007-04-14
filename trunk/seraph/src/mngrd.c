1b1c6ffa483bd3ba04d2fe4aff0e8ce3a9ff696a
groleo <groleo@01921a93-d2f6-0310-add8-e343ee640d08>
2007-04-14 07:50:17 +0000

    add copyright GPL to files

--- trunk/seraph/src/mngrd.c
+++ trunk/seraph/src/mngrd.c
@@ -1,3 +1,22 @@
+/*
+ * Copyright (C) 2006, 2007 Free Software Foundation, Inc.
+ * Written by Negreanu Marius <groleo@gmail.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program; see the file COPYING.  If not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
 /**
  *   \brief    The side that runs the scripts.
  *   \see      ptgenhost.c
