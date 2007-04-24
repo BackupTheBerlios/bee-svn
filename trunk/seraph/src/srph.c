f393d17800d4e83c9160fa23ef8fcacd7f447dc1
groleo <groleo@01921a93-d2f6-0310-add8-e343ee640d08>
2007-04-24 18:18:48 +0000

    Filenames will be named acording to their namespaces

--- trunk/seraph/src/srph.c
+++ trunk/seraph/src/srph.c
@@ -59,6 +59,7 @@ int main( int argc, char *argv[] )
 
 
     DBG("seraph.debug");
+
     if( argc == 1 )
         srph_usage( EXIT_FAILURE );
 
@@ -67,37 +68,6 @@ int main( int argc, char *argv[] )
 
     /*2. export axi_ttype axi_host axi_port specified as -t, -H, -P */
     srph_parseArgs( &cfg, argc, argv );
-    switch ( cfg.behaviour ) {
-        case TB_BE_DAEMON:
-            if( cfg.start_rawrpc ) {
-                if(cfg.port == 0 ) {
-                    fprintf( stderr,
-                            "! seraph: You have to specify -P option\n" );
-                    exit( -2 );}
-                else
-                {
-                    start_rawrpc( cfg.port );
-                }
-
-            }                /*if( cfg.start_jabber)
-                               start_jabber();*/
-            if( cfg.start_xmlrpc) {
-                /*3. export any variable from config file */
-                cfg.takeAction = setenv;
-                scan_parseCfg( cfg.config_file, (void*)1 );
-
-                /*4. check if the needed variables are exported */
-                srph_initEnv( &cfg );
-                /*5. check if the proper tools are installed */
-                srph_checkTools( getenv( SUT_TOOL ) );
-                start_xmlrpc(cfg.port);
-            }
-            return 0;
-        case TB_BE_SETUP:
-            //setup(cfg.machine);
-            return 0;
-    }
-
 
     /*3. export any variable from config file */
     cfg.takeAction = onLineParsed;
