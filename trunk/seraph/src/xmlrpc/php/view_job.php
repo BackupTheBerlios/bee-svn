b2023759d9043d7feba75ce1eac87574e0c4b589
groleo <groleo@01921a93-d2f6-0310-add8-e343ee640d08>
2007-05-03 19:46:50 +0000

    view_job didn't knew abt $_SESSION

--- trunk/seraph/src/xmlrpc/php/view_job.php
+++ trunk/seraph/src/xmlrpc/php/view_job.php
@@ -1,6 +1,7 @@
 <?php
 require_once 'XML/RPC.php';
 require_once 'base_lib.php' ;
+session_start();
 
 $cli = new XML_RPC_Client('/RPCSERVER', $_SESSION["host"], $_SESSION["port"] );
 $req = new XML_RPC_Value( $_REQUEST['log'], 'string');
